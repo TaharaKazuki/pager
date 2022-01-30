864be83baaef2a4ed34fe4d8b3381fa2cb702be4
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-30 09:05:30 +0900

    update

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -1,16 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, MouseEvent } from 'react'
+import './style.scss'
+
+interface HTMLButtonEvent extends Event {
+  target: HTMLButtonElement
+}
+
+const renderData = (data: Array<any>) => {
+  return (
+    <ul>
+      {data.map((todo, index) => (
+        <li key={index}>{todo.title}</li>
+      ))}
+    </ul>
+  )
+}
 
 const Pager = () => {
   const [data, setData] = useState([])
 
-  const [currentPage, setCurrentPate] = useState(1)
-  const [itemPerPage, setItemPerPage] = useState(5)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemPerPage] = useState(5)
 
   const [pageNumberLimit, setPageNumberLimit] = useState(5)
   const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5)
   const [mintPageNumberLimit, setMinNumberLimit] = useState(0)
 
-  const handlePrev = () => {}
+  const handleClick = (e: MouseEvent<HTMLInputElement>) => {
+    setCurrentPage(+e.target.id)
+  }
+
+  const handlePrev = (e: MouseEvent<HTMLInputElement>) => {}
+
+  const pages = []
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pages.push(i)
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = data.slice(indexOfLastItem, indexOfLastItem)
+
+  const renderPageNumbers = pages.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > mintPageNumberLimit) {
+      return (
+        <li
+          key={number}
+          id={`${number}`}
+          onClick={handleClick}
+          className={currentPage === number ? 'active' : undefined}
+        >
+          {number}
+        </li>
+      )
+    }
+  })
 
   return (
     <>
@@ -20,13 +63,11 @@ const Pager = () => {
           <button onClick={handlePrev}>Prev</button>
         </li>
         {}
-        <li>
-          <button onClick={handlePrev}>Prev</button>
-        </li>
+        <li>{/* <button onClick={handlePrev}>Prev</button> */}</li>
       </ul>
-      <button onClick={handleLoadMore} className="loadmore">
+      {/* <button onClick={(e) => handlePrev(e)} className="loadmore">
         Load More
-      </button>
+      </button> */}
     </>
   )
 }
